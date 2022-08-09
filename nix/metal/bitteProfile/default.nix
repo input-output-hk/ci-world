5eb24f7d914611bd428c32d9a230c13e0044e166
John Lotoski <john.lotoski@iohk.io>
2022-08-09 10:47:03 -0500

    maint: bump cache machine ebs gp2 vol size

--- nix/metal/bitteProfile/default.nix
+++ nix/metal/bitteProfile/default.nix
@@ -216,7 +216,7 @@ in {
           instanceType = "m5.4xlarge";
           privateIP = "172.16.0.52";
           subnet = cluster.vpc.subnets.core-1;
-          volumeSize = 600;
+          volumeSize = 1000;
 
           modules = [
             (bitte + /profiles/auxiliaries/telegraf.nix)
