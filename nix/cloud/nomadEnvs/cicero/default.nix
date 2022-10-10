input-output-hk/ci-world

commit e9dc3a148d0314ddcc33f3709b0075b96e2ce37e

cicero job: adapt transformer to new cicero version

nix/cloud/nomadEnvs/cicero/default.nix
@@ -44,28 +44,34 @@
         /bin/jq --compact-output \
           --argjson args ${lib.escapeShellArg (builtins.toJSON args)} \
           '
-            .job[]?.datacenters |= . + $args.datacenters |
-            .job[]?.group[]?.task[]? |= (
-              .vault.policies |= . + ["cicero"] |
-              .env |= . + {
-                NOMAD_ADDR: env.NOMAD_ADDR,
-                NOMAD_TOKEN: env.NOMAD_TOKEN,
-                CICERO_WEB_URL: $args.ciceroWebUrl,
-                NIX_CONFIG: ($args.nixConfig + .NIX_CONFIG),
-              } |
-              .template |= . + [{
-                destination: "local/post-build-hook",
-                perms: "544",
-                data: $args.postBuildHook,
-              }] |
-              if .driver != "nix" or .config?.nixos then . else
-                .config.packages |=
-                  # only add bash if needed to avoid conflicts in profile
-                  if any(endswith("#bash") or endswith("#bashInteractive"))
-                  then .
-                  else . + ["github:NixOS/nixpkgs/\($args.nixpkgsRev)#bash"]
+            .job |= (
+              .Datacenters += $args.datacenters |
+              .TaskGroups[]?.Tasks[]? |= (
+                .Env += {
+                  NOMAD_ADDR: env.NOMAD_ADDR,
+                  NOMAD_TOKEN: env.NOMAD_TOKEN,
+                  CICERO_WEB_URL: $args.ciceroWebUrl,
+                  NIX_CONFIG: ($args.nixConfig + .NIX_CONFIG),
+                } |
+                .Templates += [{
+                  DestPath: "local/post-build-hook",
+                  Perms: "544",
+                  EmbeddedTmpl: $args.postBuildHook,
+                }]
+              ) |
+              if .Type != null and .Type != "batch" then . else (
+                .TaskGroups[]?.Tasks[]? |= (
+                  .Vault.Policies += ["cicero"] |
+                  if .Driver != "nix" or .Config?.nixos then . else
+                    .Config.packages |=
+                      # only add bash if needed to avoid conflicts in profile
+                      if any(endswith("#bash") or endswith("#bashInteractive"))
+                      then .
+                      else . + ["github:NixOS/nixpkgs/\($args.nixpkgsRev)#bash"]
+                      end
                   end
-              end
+                )
+              ) end
             )
           '
       '';

commit a4631366acaf956fa7ae4ce940ad4c23013e1186

fix NIX_CONFIG overridden in cicero job
The bug was introduced in e9dc3a148d0314ddcc33f3709b0075b96e2ce37e
but was not noticed until 5e224bebe7c6bf6fd615a4aebd8583af42ede2f4
as that caused the nix sandbox to fail.

nix/cloud/nomadEnvs/cicero/default.nix
@@ -57,7 +57,7 @@
             .job |= (
               .Datacenters += $args.datacenters |
               .TaskGroups[]?.Tasks[]? |= (
-                .Env += {
+                .Env |= . + {
                   NOMAD_ADDR: env.NOMAD_ADDR,
                   NOMAD_TOKEN: env.NOMAD_TOKEN,
                   CICERO_WEB_URL: $args.ciceroWebUrl,