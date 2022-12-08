83277ad1bbb0f08535c35898a9c85248b9da2bd2
John Lotoski <john.lotoski@iohk.io>
2022-12-07 19:53:52 -0600

    imp: add buildkite encrypted config

---
+++ nix/metal/bitteProfile/encrypted/buildkite/buildkite-hook-extra-creds.sh
@@ -0,0 +1,22 @@
+{
+	"data": "ENC[AES256_GCM,data:ivbXYKHzttaniT3tc+8PiF978Y2iTkNon7bvrY0NEAatwjSkwEOnACDVw2HMad4u/m8xGXth2JqPldih4bW0r2KpPaS4jDtuLlcMMPSZ+qxaIwWBN/2hCX5SmazcYlB0av1/SHJ4CgGhbuQKi/2TyZiPxnMUHePsUY8sVshIHaVduwCtfR/0BCvxxbwU7GlQI4OYKJzZfDknBt/XSkWKGcvO7NXMyBuSPdd5aRx1Khbb/7vtAvUbWt4Xh7N5bi+JTG5Pry8Qgnlf//A1vHF3yZj1emRXk7rgMziEizMmFkJTXPaQmH7FO64rntQUkUvBtmZ/vwvntwVaLnl+zw9wIi3dj2RjXLEP990kbwjlK3SjWrCjteZyJJnPyex00zzj14EZgbZsNpc7Uv4v6BiuiKbT8iJdBgmKpO+z+uIpHbuBM3dzCVs58B+tXXGEMpN01u/JjpeeVQ0Uw2HaUcHe5f6s1xbgSrbGTzY/L9NPM4y1wMAcTTjCGUAXa0qwH6FQKfi+d/e9bWoETaPOvch3DUS2i4JsvKqwB6D5SN96CNuCjFXj13+HDYfAj3M4KcNgN7LrXgtAr0LlbmlIl8HwTXb7KX6yD7d6pFUTPGlad0BDZ+yXOV3kUp/Od1jfEuVoNAffMDmyUPoujoX+R/B6v3jKzpAwa2hXEt/KL1bT/643YAIvO1b5dJbU+QcTLBnaS8x+cCxLOmkf7JrETi4rqpTAEHJ9QbIlegCdn2wFEoHG1u+YSN8umd1p91XIJE+DZMmtp4fsI4mUzaYsB0gjVUWhEcT2tiwZz70K9Kemz7FAnRDIp9Dwo6+D72Rf9DQ09i+vY0EUN16bQtgkLgOlFaou42cvfNbwP2p7SXBK/8TNVR+sZDv0GjJqqbDGrjsnfgyFr4z2Ih6cBwdRpB8gpXCNXStjOYBPT5eaJNMADPjxsd+2RFrBdqePDuH5wgdXi3ounW+4HA1mPFZaeLp7a54Z6H0xpiuRAc1KjyDB8qr4SKjI10eJtbiUZI/dkLg+aENB5WFamN1898ZxOreUkbiex8Tyz7SnLisUVKxxaFBEQee+cRzqeH6F+ZxgdN4PELPDlvL8owkeVm6zoqmwomX8QdoH6I50Op+iVVEU2o5YdJAelJm3V0w54tMVUI9fkK9x9twEka3qwdeAe9hoZz4HQslNBDSvAi7pO6f08KTOte+mKSfOyCv9oRkCdrY/pRcqCzGBmiA5iH6Tr6i89jFyvyhbqHyKee0xkjdk4M+2EIQPsRbBLLxj8qxB/zQSms8Ntp/2rFcT3JCbFWdh8KIeSnWp33tKGLx4ad7nIpgWr3sNugEXKW85bpkwXyh8WRvZX6JtGW7WWqd9b/cwi92O0g0JgrcehNI9uj+1nDGm6wL9LfERUFnS5rs8P4L5inbFeP0xkF/HDOluaFOsfKYKJH+H/ArmLCJ27/WgV/k+isOcqJ4IP4nrDHD5vlnwyMEA3qfTDRK5zTq3+GLO2PUKPHC+d8Y1ImXSGXZ841RQBiVVwwp4W78uF8Th33JyDED3h5+BaqT3cRi+HsptL7NwJ/SJpZFGzftY6wjlS1QODdn6XAM4KJl46vZQfqrGSyv3Sj5kIJeVHSHl4dZ3gpuPf9QrtqQngoCuVGs64D6OtRJmhUBJKl9VkdnwXgbd1eqnfG6GYzZ7Gwkh94gcNkcEsXd3uBbFpHFwlt445lnBggwDNRKUD3PAnN8O1zs0/gnI6m6CapGWv1CkFxRTK/qz+LUuW0oA7y6MX+byqjRQS46NB3AuYEYZt541KJIi95MEi7nMg6Yho91Osxdqo3Rg4tk2v8caejvcuvmnNDb5lUIUc7OFAbBXqsbI4WD+V7nJ8cg9QPZwxh+Pewf9Ad4wFP3TKZF7Mn8xYrdNKthCADwc8mDQqrUxuHW9xJD8u7/uTg+0o/uISNoT1FMOSwO94oJfPmZwu7Q5QWpdcMVsULIhHtJyARuBuul0oNcPEsYnhG6NKFs6+Y1xaCZnFatPxaSOtZ7z+fk+s5PDXkCcqbRg+KaTmru/yqPdbG8SybxbBuxRdBB77L5mteT/vtgJoyq9cNcde/kh7CnY8pc6Z/oIF3pwJ5/LyZjydFndoJE0hrILi06IgjLnt+VBWZw7+AQgDPjUyNql7k5lH8TqFnKT/SPv37Chiy2wtLqpKqLgshQi01NQWIex3i6KMBBN4tQ78vfg6ztZJw5KClt49JBucZdpeCnnVrjRmMHwAjfEcpUW2HSGMLEQDP47JkmqgECZ4Xp1Qvqje1JNWPtZxek=,iv:ephpJZu/DMHsihh+Lg+UDKSGn0stfD5tKoztEn7Qyvo=,tag:m7qbCFK7lVw0bdoZPT8YEA==,type:str]",
+	"sops": {
+		"kms": [
+			{
+				"arn": "arn:aws:kms:eu-central-1:074718059002:key/5bb7cc1b-151c-4841-bcb3-622bc8df4b5a",
+				"created_at": "2022-12-07T20:31:10Z",
+				"enc": "AQICAHg3OCn+/PAIR22bdMZmmm7xlk3KpM4n+haJ8Ph2IaYRgwHfbqr45nFl/VeRwwgQs/z1AAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMFAoKqsvkCGd9n3qKAgEQgDuDi6MG8JAB1o3Gwgk8w/IzzSv3K2uUJI2LjiqjMzmpT5UbEpnmdnD9ocQyYDwHztvMbTVk/5oq6N+zoQ==",
+				"aws_profile": ""
+			}
+		],
+		"gcp_kms": null,
+		"azure_kv": null,
+		"hc_vault": null,
+		"age": null,
+		"lastmodified": "2022-12-07T20:31:11Z",
+		"mac": "ENC[AES256_GCM,data:rMW3wbV1Xd9sc10MagDaoavGeXl4CYmc1+4c3JTvQNZC8wkmD8l5vLQPI7kdXSOK9eXDIsm+18q/qNLDmCxtPCgDup/DmVxX/wfvMnp5QcsqxZytwRslQ5IVorYA7NkRkbytJkXCB+EMuzLt78AIJyxuMH/b+QbobJ/HAdg4WE0=,iv:xaE693/+0XNnamqJu/n9YNFP6fTcRr5FCYfQdzDi4z4=,tag:t7WXqbC6TdT0JsbivQWcOQ==,type:str]",
+		"pgp": null,
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.7.3"
+	}
+}
---
+++ nix/metal/bitteProfile/encrypted/buildkite/catalyst-env.sh
@@ -0,0 +1,22 @@
+{
+	"data": "ENC[AES256_GCM,data:PwtxWQI95WzK+1+0+2bXW4Zfv1sYVErHuGweqdMBWOOV/jhJCXCxFdPOG5wS3AUdfr0EuBf/XaRnPmRTwD8wVmiBkv0aFRARpu/w67oJU2OEswG6leuA/bfADoozvXhE5za1h4Q1kzyqB0fLV1ls7Bn183pbu68oKWtc5Df/1RagaekZdikf3Z6ai/J/L/YnDwm8VBrtzZvQNmvQv9kANhAA9SrKQzf6nOMyRpDtePuxuR8NePGCkYsvCYtrWDYcZAFWb95g9+DYt58e2EleyoVfTxIgM2hzkbBEeq4ehP8cFU/IgA==,iv:ZNPK0znCbNH9eIJYcRqfu2ax/3c2A+Xpqcn+MREmmqI=,tag:ZkOOLmru/0QSVkDkPh94mQ==,type:str]",
+	"sops": {
+		"kms": [
+			{
+				"arn": "arn:aws:kms:eu-central-1:074718059002:key/5bb7cc1b-151c-4841-bcb3-622bc8df4b5a",
+				"created_at": "2022-12-07T21:41:33Z",
+				"enc": "AQICAHg3OCn+/PAIR22bdMZmmm7xlk3KpM4n+haJ8Ph2IaYRgwEEeRNxknkr6eFt+ITAOao1AAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQM6EWJw50NL3lnNU8tAgEQgDubnRMBY6sXdDnImRT4lSv++oenDViKSrsrgRUS6U8kFSQ8A+gPsxqJI2XAitFJEnxWCVZ/fcMjqFuy5w==",
+				"aws_profile": ""
+			}
+		],
+		"gcp_kms": null,
+		"azure_kv": null,
+		"hc_vault": null,
+		"age": null,
+		"lastmodified": "2022-12-07T21:41:34Z",
+		"mac": "ENC[AES256_GCM,data:dRic3ITd0h6VZ2jcN69rOGstt4srJRzcm5VFdvkt7fm3Brp5z5I0RVaAxu9xMbc47UsMENRLZkVpDFBVZsZiwraGmV8/OCgZtDdyi243z12mOIJsNJhsA4H/kk/S9jDSW9V4dKrCjrnHhTErYF3RPcU2fDfQTZRYnp7OAuxj0CU=,iv:n4Xqj4ZWY/XbL0cgtjBYsBAxi+T8Dtc2Ql4sd64Q660=,tag:xw/pNqUiVJ9Cdzsb6mJjDA==,type:str]",
+		"pgp": null,
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.7.3"
+	}
+}
