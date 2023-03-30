c4bc0934844341e4659e5abc1427c36da712b09e
John Lotoski <john.lotoski@iohk.io>
2023-03-29 22:47:41 -0500

    imp: make sure arm guests get rosetta and cleanup service

--- nix/metal/bitteProfile/darwin/guests/arch/aarch64-darwin.nix
+++ nix/metal/bitteProfile/darwin/guests/arch/aarch64-darwin.nix
@@ -16,4 +16,27 @@ in {
     system = aarch64-darwin
     extra-platforms = aarch64-darwin x86_64-darwin
   '';
+
+  system.activationScripts.postActivation.text = ''
+    # Ensure Rosetta is installed for x86_64 emulation on apple silicon
+    printf "checking for rosetta... "
+    if ! [ $(/usr/bin/pgrep oahd) ]; then
+      echo "installing"
+      softwareupdate --install-rosetta --agree-to-license || true
+    else
+      echo "already installed"
+    fi
+  '';
+
+  # Clean up generated AOT x86_64 emulation byproduct files
+  launchd.daemons.cleanup-rosetta-cache = {
+    script = ''
+      /System/Library/Filesystems/apfs.fs/Contents/Resources/apfs.util -P -minsize 0 /System/Volumes/Data/private/var/db/oah
+    '';
+
+    serviceConfig = {
+      # Run every hour at the 24th minute
+      StartCalendarInterval = [{Minute = 24;}];
+    };
+  };
 }
