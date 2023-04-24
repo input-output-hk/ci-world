0684342ac645d02ccd3383799ee3202a70ab89c9
Robin Stumm <robin.stumm@iohk.io>
2023-04-24 15:07:56 +0200

    remove commented-out code

--- flake.nix
+++ flake.nix
@@ -116,7 +116,6 @@
   nixConfig = {
     extra-substituters = ["https://cache.iog.io"];
     extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
-    # post-build-hook = "./upload-to-cache.sh";
     allow-import-from-derivation = "true";
   };
   # --------------------------------------------------------------
