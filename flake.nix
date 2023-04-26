d45fce41a0d458d7fc0a3e0d5e791c04650b2aae
John Lotoski <john.lotoski@iohk.io>
2023-04-25 20:39:48 -0500

    imp: bump nix to 2.15.1 and cut max-jobs to 2

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     # --------------------------------------------------------------
     # --- Auxiliary Nixpkgs ----------------------------------------
     nixpkgs.follows = "bitte/nixpkgs";
-    nix.url = "github:NixOS/nix/2.13-maintenance";
+    nix.url = "github:NixOS/nix/2.15-maintenance";
     capsules = {
       # Until nixago is implemented, as HEAD currently removes fmt hooks
       url = "github:input-output-hk/devshell-capsules/8dcf0e917848abbe58c58fc5d49069c32cd2f585";
--- nix/metal/bitteProfile/darwin/guests/flake.nix
+++ nix/metal/bitteProfile/darwin/guests/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-22.11-darwin";
-    nix.url = "github:NixOS/nix/2.13-maintenance";
+    nix.url = "github:NixOS/nix/2.15-maintenance";
     darwin = {
       url = "github:lnl7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
