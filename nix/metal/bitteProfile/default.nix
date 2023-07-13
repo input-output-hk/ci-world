023d92a21b25e26339768aa1d70be710b01d7431
Robin Stumm <robin.stumm@iohk.io>
2023-07-13 18:06:16 +0200

    shut down perf node

--- nix/metal/bitteProfile/default.nix
+++ nix/metal/bitteProfile/default.nix
@@ -149,7 +149,7 @@ in {
             (mkAsgs "eu-central-1" 3 "t3.xlarge" 200 "infra" "infra" {withPatroni = true;} {volumeType = "gp3";})
             (mkAsgs "eu-central-1" 0 "m5.metal" 1000 "baremetal" "baremetal" {} {primaryInterface = "enp125s0";})
             (mkAsgs "eu-central-1" 0 "t3a.medium" 100 "test" "test" {} {})
-            (mkAsgs "eu-central-1" 1 "t3a.medium" 100 "perf" "perf" {} {})
+            (mkAsgs "eu-central-1" 0 "t3a.medium" 100 "perf" "perf" {} {})
           ]
           (args: let
             attrs =
