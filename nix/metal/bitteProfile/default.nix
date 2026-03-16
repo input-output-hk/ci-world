8ab18b709863158e189f013c4851a559f11925ec
John Lotoski <john.lotoski@iohk.io>
2026-03-16 09:39:12 -0500

    equinix: shut down equinix-7

--- nix/metal/bitteProfile/default.nix
+++ nix/metal/bitteProfile/default.nix
@@ -705,12 +705,12 @@ in {
         #     queue = ["adrestia-bench"];
         #   } ["Billing:team-adrestia"]
         #   1 {};
-        equinix-7 =
-          mkEquinixBuildkite "equinix-7" "7" "10.12.10.13" {
-            system = "x86_64-linux";
-            queue = ["catalyst"];
-          } ["Billing:team-catalyst"]
-          5 {};
+        # equinix-7 =
+        #   mkEquinixBuildkite "equinix-7" "7" "10.12.10.13" {
+        #     system = "x86_64-linux";
+        #     queue = ["catalyst"];
+        #   } ["Billing:team-catalyst"]
+        #   5 {};
       };
     };
   };
